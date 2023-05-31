c35bfb97f15db4645f756167ae6a1c2e59d0b1f2
RamaRamdani03 <ramaramdani449@gamil.com>
2023-05-31 21:08:02 +0700

    class abstraksi dengan hak access public dengan mengisi nilai dan menampilkan nilai

--- program abstraksi/program abstraksi.cpp
+++ program abstraksi/program abstraksi.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 class Absrtaksiklas {
-private: string x, y;
+private: string x, y; 
+
+public:
+
+	// method untuk mengisi nilai
+	//private member
+	void setXY(string a, string b) {
+		x = a;
+		y = b;
+	}
+	// menampilkan nilai
+	void display() {
+		cout << "x = " << x << endl;
+		cout << "y = " << y << endl;
+	}
 };
