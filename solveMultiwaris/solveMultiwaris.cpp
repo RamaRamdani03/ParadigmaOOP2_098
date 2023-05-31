683555cbbd7b93d38d2912c0b0763ccc475cd2d6
RamaRamdani03 <ramaramdani449@gamil.com>
2023-05-31 20:13:42 +0700

    class pelajar dengan anak dari virtual public orang dengan hak access public yang berisikan umur pekerja dan orang dengan menggunakan integer yang akan menampilkan pelajar dibuat

--- solveMultiwaris/solveMultiwaris.cpp
+++ solveMultiwaris/solveMultiwaris.cpp
@@ -19,5 +19,15 @@ public:
 		orang(pUmur)
 	{
 		cout << "pekerja dibuat\n" << endl;
+	} 
+};
+
+class pelajar : virtual public orang {
+public:
+
+	pelajar(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pelajar dibuat\n" << endl;
 	}
 };
