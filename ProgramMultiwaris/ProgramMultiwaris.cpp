fb366992f9b65ac333f64eb05ab4406a9fd13742
RamaRamdani03 <ramaramdani449@gamil.com>
2023-05-31 19:44:26 +0700

    class manusia dengan hak access public dengan menggunkan string yang akan menghasilkan cout manusia dibuat atau manusia dihapus

--- ProgramMultiwaris/ProgramMultiwaris.cpp
+++ ProgramMultiwaris/ProgramMultiwaris.cpp
@@ -13,4 +13,18 @@ public:
 	~orang() {
 		cout << "orang yang dihapus\n" << endl;
 	}
+}; 
+
+class manusia {
+public:
+	string JenisKelamin;
+
+	manusia(string pJenisKelamin) :
+		JenisKelamin(pJenisKelamin) {
+		cout << "Manusia dibuat\n" << endl;
+	}
+	~manusia() {
+		cout << "Manusia dihapus\n" << endl;
+	}
+
 };
